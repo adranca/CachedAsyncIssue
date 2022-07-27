66dcbcf79f3905bcd5ae22949c822bc98e48e898
Alexandru Dranca <adranca@specialized.com>
2022-07-27 10:21:32 +0300

    Initial Commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Alexandru Dranca on 27.07.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/TestImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestImageApp.swift
+//  Shared
+//
+//  Created by Alexandru Dranca on 27.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct TestImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
